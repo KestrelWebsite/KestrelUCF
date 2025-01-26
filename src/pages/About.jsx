06ff33113127395346dee34c511f6a51dfaa5bac
amersamman <amersamman2@gmail.com>
2025-01-25 23:37:23 -0500

    Added Pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+//About Page
+import React from "react";
+
+export default function About(){
+    return(
+        <>
+            <div>
+                <h1>hello</h1>
+            </div>
+        </>
+    )
+}
+
