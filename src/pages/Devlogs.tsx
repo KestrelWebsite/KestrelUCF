1e9a92a92a5c5a707cbebf2de8e2e26475f640f0
JylerTackson <dtj1029@gmail.com>
2025-04-06 18:03:51 -0400

    Dynamic Child Elements for Devlogs
    Attempting to create a child elements folder for the Devlogs webpage that will pull all Devlog.tsx files from a specified folder and dynamically create each devlog based on a template.

--- src/pages/Devlogs.tsx
+++ src/pages/Devlogs.tsx
@@ -1,5 +1,29 @@
-const Devlogs = () => {
-  return <div>DEV LOG</div>;
+import React from 'react';
+
+// For a synchronous (eager) import:
+const modules = import.meta.glob('./components/Devlogs/createdDevlogs*.tsx', { eager: true });
+
+// "modules" is now an object keyed by file path, whose values
+// are each the module’s exports. Something like:
+// {
+//   './devlog/Devlog1.tsx': { default: [Function Devlog1], ... },
+//   './devlog/Devlog2.tsx': { default: [Function Devlog2], ... }
+// }
+
+const Devlogs: React.FC = () => {
+  // Convert the modules object into an array of [path, module]
+  const devlogEntries = Object.entries(modules);
+
+  return (
+    <div>
+      <h1>All Devlogs</h1>
+      {devlogEntries.map(([path, module]) => {
+        // We assume each devlog file exports a React component as default
+        const DevlogComponent = (module as any).default;
+        return <DevlogComponent key={path} />;
+      })}
+    </div>
+  );
 };
 
 export default Devlogs;
